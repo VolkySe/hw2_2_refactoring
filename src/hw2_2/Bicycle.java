c687c20c98bf14c5a2da4b8ef186c2e656e19aad
VolkySe <sergej_lykov@mail.ru>
2025-03-09 10:35:04 +0300

    Source code copied from lesson

---
+++ src/hw2_2/Bicycle.java
@@ -0,0 +1,10 @@
+package hw2_2;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
